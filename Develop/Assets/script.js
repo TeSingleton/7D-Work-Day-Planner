21535f8ec5da276575afd604b8a1f295647d8c5e
Te <smoochbanner@icloud.com>
2022-07-05 00:22:13 -0700

    adding updated files

---
+++ Develop/Assets/script.js
@@ -0,0 +1,59 @@
+// console.log('hello from file');
+console.log('im on it.');
+
+// formatting and code help from dcode on youtube https://youtu.be/nmAHDEO9RW8
+const today = moment();
+$("#currentDay").text(today.format("MMMM Do YYYY, h:mm:ss a"));
+
+console.log(today.format("MMMM Do YYYY, h:mm:ss a"));
+
+
+
+
+//Declare Current time
+
+//Declare Current Day Element
+
+//Declare Time Entries Container Element
+
+//Render a block for each hour of the day
+    //add a for loop with array to log the times on the planner. 
+    // for(var i=9; i<= 17; i++){
+
+    // };  // for example
+
+
+//save an hour to local storage 
+
+// Html needs a data attribute for loggin the time.
+
+
+// --------------------------------
+
+
+
+
+
+
+
+// below solution from Knowledge Base on YouTube https://youtu.be/CJSukyN5gBc
+// Instead of copying the div in the HTML, I Used JS to clone the divs as needed. 
+
+
+// original copy code below
+
+// function duplicate (){
+// const dupeDiv = document.getElementById ("dupe");
+
+// const divClone = dupeDiv.cloneNode (true);
+
+// // };
+// // "true is for deep cloning"
+
+// document.body.appendChild(divClone);
+
+
+
+// console.log(divClone)
+
+
