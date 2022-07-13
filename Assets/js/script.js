1313cf881a9d8dcee9720a0fb9a26f1c5c160bf5
Dope Undawater <worksingletonmcdaniel@gmail.com>
2022-07-13 00:29:09 -0700

    updates to js, added highlight function

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,56 +1,72 @@
-// console.log('hello from file');
-console.log('im on it.');
+// // console.log('hello from file');
+// console.log('im on it.');
 
-const scheduled = document.querySelector(".saveBtn")
+// const scheduled = document.querySelector(".saveBtn")
+// console.log(scheduled)
+// const scheduledInput = document.querySelector("textarea")
+// console.log(scheduledInput)
+// const today = moment();
+// console.log(today.format("MMMM Do YYYY, h:mm:ss a"));
 
-// formatting and code help from dcode on youtube https://youtu.be/nmAHDEO9RW8
-const today = moment();
-$("#H-Time").text(today.format("MMMM Do YYYY, h:mm:ss a"));
-
-console.log(today.format("MMMM Do YYYY, h:mm:ss a"));
-
-for(var i=9; i<= 17; i++){
-
-  // attempt to get the saved data for the hour of the loop
-var data = "" ;
-
-// Compare i to current hour to determine if this hour is in the past, present, future. 
-var template = `
-<div class="row">
-        <div class="col-1 time-block hour">${i}</div>
-        <textarea class="col-10 past present future">
-          ${data}
-        </textarea>
-        <button data-hour="${i}" class="col-1 saveBtn"><i class="fas fa-save fa-x"></i></button>
-      </div>
-      `;
-      // Appened the HTML
-};
+// localStorage.setItem('textarea')
+// console.log(localStorage)
 
 
 
+// // formatting and code help from dcode on youtube https://youtu.be/nmAHDEO9RW8
 
-//Declare Current time
-
-//Declare Current Day Element
-
-//Declare Time Entries Container Element
-
-//Render a block for each hour of the day
-    //add a for loop with array to log the times on the planner. 
-    // for(var i=9; i<= 17; i++){
-
-    // };  // for example
-
-
-//save an hour to local storage 
-
-// Html needs a data attribute for loggin the time.
+// $("#H-Time").text(today.format("MMMM Do YYYY, h:mm:ss a"));
 
 
 
+// for(var i=9; i<= 17; i++){
 
-// --------------------------------
+//   // attempt to get the saved data for the hour of the loop
+// var key = "hour-"+i;
+// var data = "" ;
+
+// // Compare i to current hour to determine if this hour is in the past, present, future. 
+// var template = `
+// <div class="row">
+//         <div class="col-1 time-block hour">${i}</div>
+//         <textarea class="col-10 past present future">
+//           ${data}
+//         </textarea>
+//         <button data-hour="${i}" class="col-1 saveBtn"><i class="fas fa-save fa-x"></i></button>
+//       </div>
+//       `;
+//       // Appened the HTML
+// };
+
+// scheduled.addEventListener('click',function(e){
+//     if(e.target.className == '.saveBtn'){
+//         // const 
+//     };
+// });
+    
+
+// localStorage(key,data);
+// //Declare Current time
+
+// //Declare Current Day Element
+
+// //Declare Time Entries Container Element
+
+// //Render a block for each hour of the day
+//     //add a for loop with array to log the times on the planner. 
+//     // for(var i=9; i<= 17; i++){
+
+//     // };  // for example
+
+
+// //save an hour to local storage 
+
+// // Html needs a data attribute for loggin the time.
+
+
+
+
+// // --------------------------------
 
 
 
@@ -58,24 +74,24 @@ var template = `
 
 
 
-// below solution from Knowledge Base on YouTube https://youtu.be/CJSukyN5gBc
-// Instead of copying the div in the HTML, I Used JS to clone the divs as needed. 
+// // below solution from Knowledge Base on YouTube https://youtu.be/CJSukyN5gBc
+// // Instead of copying the div in the HTML, I Used JS to clone the divs as needed. 
 
 
-// original copy code below
+// // original copy code below
 
-// function duplicate (){
-// const dupeDiv = document.getElementById ("dupe");
+// // function duplicate (){
+// // const dupeDiv = document.getElementById ("dupe");
 
-// const divClone = dupeDiv.cloneNode (true);
+// // const divClone = dupeDiv.cloneNode (true);
 
-// // };
-// // "true is for deep cloning"
+// // // };
+// // // "true is for deep cloning"
 
-// document.body.appendChild(divClone);
+// // document.body.appendChild(divClone);
 
 
 
-// console.log(divClone)
+// // console.log(divClone)
 
 
